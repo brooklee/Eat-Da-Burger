09478e8c550db7f014d2864c0ce310bad863a1a8
brooklee1 <brooklee.wilson@gmail.com>
2017-10-01 12:23:14 -0600

    fix public

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,22 @@
-const orm = require("../config/orm.js");
 
-const burger = {
-    selectAll: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
-    },
-    insertOne: function (cb) {
-        orm.insertOne("burgers",cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    updateOne: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    }
+var orm = require('../config/orm.js');
+
+var burger = {
+	all: function (cb) {
+		orm.selectAll('burgers', function (res) {
+			cb(res);
+		});
+	},
+	create: function (col, burger_name, cb) {
+		orm.insertOne('burgers', burger_name, function (res) {
+			cb(res);
+		});
+	},
+	update: function (objColVals, condition, cb) {
+		orm.updateOne('burgers', objColVals, condition, function (res) {
+			cb(res);
+		});
+	}
 };
 
 module.exports = burger;
