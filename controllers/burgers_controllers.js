17cd31d2baffa5508b6f61ebcf10f8da591a2784
brooklee1 <brooklee.wilson@gmail.com>
2017-10-20 22:32:12 -0600

    Burgers connections remove req

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -10,11 +10,11 @@ var methodOverride = require('method-override');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
-router.get('/', function (req, res) {
+router.get('/', function (res) {
 	res.redirect('/burgers');
 });
 
-router.get('/burgers', function (req, res) {
+router.get('/burgers', function (res) {
 	burger.all(function (data) {
 		var hbsObject = { burgers: data };
 		// console.log(hbsObject);
