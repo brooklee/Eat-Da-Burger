e45c13b6e6ccc43e30730ae4ac36f52c88aec2a4
brooklee1 <brooklee.wilson@gmail.com>
2017-10-20 23:21:08 -0600

    remove server function

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ var app = express();
 
 var PORT = process.env.PORT || 8081;
 
-server.listen(PORT, function() {
-    console.log("App is running on port " + PORT);
-});
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
 
