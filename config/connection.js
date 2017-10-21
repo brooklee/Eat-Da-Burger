3e5b168771dea83769fe7830c589d6bb441d194b
brooklee1 <brooklee.wilson@gmail.com>
2017-10-20 22:03:54 -0600

    revert  DB Connections

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,16 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
     port: 3306,
-    host: "us-cdbr-iron-east-05.cleardb.net",
-    user: "ba614281ff3063",
-    password: "8ec64b64",
-    database: "heroku_cb581b6cdbc1be9"
+    host: "localhost",
+    user: "root",
+    password: "cangetin",
+    database: "burgers_db"
+
+    // port: 3306,
+    // host: "us-cdbr-iron-east-05.cleardb.net",
+    // user: "ba614281ff3063",
+    // password: "8ec64b64",
+    // database: "heroku_cb581b6cdbc1be9"
 });
 
 // Make connection.
